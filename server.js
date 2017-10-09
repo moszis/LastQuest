db08c60b0063d3ddaeb6176f7eb11f0f37378ba1
moszis <moszis@yahoo.com>
2017-10-09 08:37:35 -0400

    Revert "Revert "Testing responsiveness""
    This reverts commit 7a046999bc6cbd9d8bf8f1d2d30b96fa0b71aec5.

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 // Dependencies
-var express = require('express');
-var http = require('http');
-var path = require('path');
+var express  = require('express');
+var http     = require('http');
+const path   = require('path');
 var socketIO = require('socket.io');
+
 var app = express();
 var server = http.Server(app);
 var io = socketIO(server);
+
 app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/client', express.static(__dirname + '/client'));
+
+
 // Routing
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, 'client/index.html'));
 });
+
 // Starts the server.
 server.listen(5000, function() {
   console.log('Starting server on port 5000');
